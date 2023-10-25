f2a84518a1ece133c300491c8e12bbcea9a3dab0
wallaceHwang0619 <wallacehwang0619@gmail.com>
2023-10-25 13:23:19 -0700

    Data.js Improvements
    Added in sorting optionality to "transactionID", "total", and "fees" to the table. There is also now styling on the component for better visuals.

--- client/src/pages/Data.js
+++ client/src/pages/Data.js
@@ -4,6 +4,9 @@ import "./Data.css";
 const Data = () => {
   const [jsonData, setJsonData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [sort, setSort] = useState({});
+
+  const types = ["transactionID, hash, total, fees, inputs, outputs"];
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,11 +33,11 @@ const Data = () => {
     };
   }, []);
 
-  const sortJson = (prop) => {
-    console.log(prop);
-    console.log("before", jsonData);
-    setJsonData(jsonData.slice().sort((a, b) => a[`${prop}`] - b[`${prop}`]));
-    console.log("after", jsonData);
+  const sortJson = (v) => {
+    setSort({
+      value: v,
+      ascending: sort.value === v ? !sort.ascending : false,
+    });
   };
 
   if (loading) {
@@ -42,33 +45,49 @@ const Data = () => {
   }
   if (jsonData) {
     return (
-      <div>
+      <div className="data-container">
         <h1>Largest Unconfirmed Transaction(s)</h1>
         <table>
           <thead>
             <tr>
-              <th onClick={(e) => sortJson("transactionID")}>transactionID</th>
-              <th onClick={(e) => sortJson("hash")}>hash</th>
-              <th onClick={(e) => sortJson("total")}>total</th>
-              <th onClick={(e) => sortJson("fees")}>fees</th>
-              <th onClick={(e) => sortJson("inputs")}>inputs</th>
-              <th onClick={(e) => sortJson("outputs")}>outputs</th>
+              <th
+                className="hoverable"
+                onClick={(e) => sortJson("transactionID")}
+              >
+                transactionID
+                {sort.value === "transactionID"
+                  ? sort.ascending
+                    ? " ↑"
+                    : " ↓"
+                  : ""}
+              </th>
+              <th>hash</th>
+              <th className="hoverable" onClick={(e) => sortJson("total")}>
+                total
+                {sort.value === "total" ? (sort.ascending ? " ↑" : " ↓") : ""}
+              </th>
+              <th className="hoverable" onClick={(e) => sortJson("fees")}>
+                fees
+                {sort.value === "fees" ? (sort.ascending ? " ↑" : " ↓") : ""}
+              </th>
+              <th>inputs</th>
+              <th>outputs</th>
             </tr>
           </thead>
           <tbody>
-            {jsonData
-              .slice()
-              .reverse()
-              .map((item) => (
-                <tr key={item.transactionID}>
-                  <td>{item.transactionID}</td>
-                  <td>{item.hash}</td>
-                  <td>{item.total}</td>
-                  <td>{item.fees}</td>
-                  <td>{JSON.stringify(item.inputs)}</td>
-                  <td>{JSON.stringify(item.outputs)}</td>
-                </tr>
-              ))}
+            {(sort.ascending
+              ? jsonData.slice().sort((a, b) => a[sort.value] - b[sort.value])
+              : jsonData.slice().sort((a, b) => b[sort.value] - a[sort.value])
+            ).map((item) => (
+              <tr key={item.transactionID}>
+                <td>{item.transactionID}</td>
+                <td>{item.hash}</td>
+                <td>{item.total}</td>
+                <td>{item.fees}</td>
+                <td>{JSON.stringify(item.inputs)}</td>
+                <td>{JSON.stringify(item.outputs)}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
